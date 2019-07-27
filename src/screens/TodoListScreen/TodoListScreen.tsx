102b8a6daf2db9bd150ec0963bd0c39cf5692268
safeheaded <m.patryk@outlook.com>
2019-07-27 12:08:54 +0200

    Usunięty module-resolver

--- src/screens/TodoListScreen/TodoListScreen.tsx
+++ src/screens/TodoListScreen/TodoListScreen.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { DrawerActions } from 'react-navigation';
 
-import SideDrawerToggleButton from 'UI/SideDrawerToggleButton/SideDrawerToggleButton';
+import SideDrawerToggleButton from '../../components/UI/SideDrawerToggleButton/SideDrawerToggleButton';
 
 // FIXME:
 interface props{
